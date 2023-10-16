f4080f4fb2b15d9a864ad594af7dc16ca10ec0c7
dreaMykj <gabrielgreghi2018@gmail.com>
2023-10-16 14:17:26 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿bool deveria,estaSeMovendo;
+string mensagem;
+
+Console.WriteLine("--- Fluxograma Da Engenharia ---");
+
+Console.Write("O objeto está se movendo (S/N)? ");
+estaSeMovendo = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.Write("Deveria? ");
+deveria = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+if (!estaSeMovendo)
+{
+    if (!deveria)
+    {
+        mensagem = "Ótimo";
+    }
+    else
+    {
+        mensagem = "Use WD-40";
+    }
+}
+else
+{
+    if (deveria)
+    {
+        mensagem = "Ótimo";
+    }
+    else
+    {
+        mensagem = "Use Silver Tape";
+    }
+}
+Console.WriteLine(mensagem);
